f33610510104d78b635240428063413d49f46a38
tongshuowen <2965574560@qq.com>
2023-11-07 18:30:23 +0800

    更改

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // devServer:{
-  //   port:7000
-  // }
+  devServer:{
+    port:7000
+  }
 })
